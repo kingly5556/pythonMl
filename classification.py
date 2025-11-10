5ddca3396a535c232630279a809766f9ffab68f7
kingly5556 <83743078+kingly5556@users.noreply.github.com>
2025-11-10 11:32:08 +0700

    Update classification.py

--- classification.py
+++ classification.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,classification_report
 import pandas as pd
 
 # def sigmoid(z):
@@ -19,10 +19,22 @@ import pandas as pd
 np.random.seed(42)
 n_samples = 200
 x = np.random.rand(n_samples,2)*10
-y = (x[:,0]*1,5+x[:,1]>15).astype(int)
+y = (x[:,0]*1.5+x[:,1]>15).astype(int)
 
 #create dataframe
 df = pd.DataFrame(x,columns=['Age','Salary'])
 df['Purchase'] = y
 
 x_train,x_test,y_train,y_test = train_test_split(df[['Age','Salary']],df['Purchase'],test_size=0.2,random_state=42)
+
+#logistic model
+model = LogisticRegression()
+model.fit(x_train,y_train)
+
+y_pred = model.predict(x_test)
+
+print("Acc:",accuracy_score(y_test,y_pred))
+print("Prec:",precision_score(y_test,y_pred))
+print("Rec:",recall_score(y_test,y_pred))
+print("F1:",f1_score(y_test,y_pred))
+print("\nClassification Report\n:",classification_report(y_test,y_pred))
